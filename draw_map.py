67e7f1e8f58a52d6f12f8bb9fef2ab4bdb50869a
Yeongching Tarn <yeongching@tarn.space>
2017-12-14 20:50:08 +0800

    draw_map.py: better subplot

--- draw_map.py
+++ draw_map.py
@@ -13,11 +13,12 @@ import model
 
 def draw(x, path, size):
     fig, axs = plt.subplots(nrows=size, ncols=size)
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0, hspace=0)
     for i in range(size):
         for j in range(size):
             axs[i][j].matshow(np.reshape(x[i * size + j], [28, 28]), cmap='Greys')
             axs[i][j].axis('off')
-    plt.savefig(path)
+    plt.savefig(path, dpi=1024)
     plt.close()
 
 
@@ -36,7 +37,7 @@ global_step = tf.Variable(0, name='global_step', trainable=False)
 
 
 cs = []
-size = 16
+size = 32
 for i in range(size):
     for j in range(size):
         cs.append([i / (size-1), j / (size-1)])
@@ -52,9 +53,9 @@ with tf.Session() as sess:
 
     eval_x_, step = sess.run([x_, global_step], feed_dict={c: cs})
 
-    path = os.path.join('tmp', args.model, str(step))
+    path = os.path.join('tmp', args.model)
     if not os.path.exists(path):
         os.makedirs(path)
-    path = path + '.png'
+    path = os.path.join(path, str(step) + '.png')
     draw(eval_x_, path, size)
     print('map saved to', path)
